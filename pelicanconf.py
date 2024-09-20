3e797d7e66c4a060cdd4f0da1a554c4d1982bc26
iceman121 <shashwatpat@gmail.com>
2024-09-19 21:08:37 -0700

    Home Page

---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+AUTHOR = 'Shashwat Pathak'
+SITENAME = 'AI, Python and Blackjack'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = 'en'
+DISPLAY_CATEGORIES_ON_MENU = True
+USE_FOLDER_AS_CATEGORY = True
+THEME = "../pelican-themes/blue-penguin-dark"
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+)
+
+# Social widget
+SOCIAL = (
+    ("Linkedin", "https://linkedin.com/in/shashwatpathak"),
+)
+
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
